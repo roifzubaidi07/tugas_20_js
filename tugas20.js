a8a054ce27bd1d0a5e9a3a7c97268cd8ef0335e9
M. Ro'if <roifzubaidi5@gmail.com>
2021-02-21 03:50:45 +0700

    pengumpulan tugas 20 JS NIOMIC

---
+++ tugas20.js
@@ -0,0 +1,3 @@
+var isi = `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`;
+console.log(isi.match('are'));
+console.log(isi.match('words'));
